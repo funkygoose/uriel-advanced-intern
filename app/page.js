9cf44d7e12335dfdb18b516b60ea44f19287be55
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2024-02-04 14:29:11 -0800

    added the html and css onto proper nextjs folders

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import HomePage from "@/components/homepage/page";
+import Navbar from "@/components/navbar/page";
+export default function Home() {
+  return (
+    <div>
+    <HomePage/>
+    </div>
+  );
+}
